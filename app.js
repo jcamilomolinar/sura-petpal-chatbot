c58a3dad45759e9b5daa893faee7d648ec5b38a0
jcamilomolinar <jmolinaro@unal.edu.co>
2025-01-26 21:00:10 -0500

    chore: fix format

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const MockAdapter = require('@bot-whatsapp/database/mock')
 // Flujo de agendar cita
 
 const flowAgendar = addKeyword(['agendar']).addAnswer(
-        'Aqui podras agendar una cita para tu mascota'
-)
+    'Aqui podras agendar una cita para tu mascota'
+);
 
 // Flujo para emergencias
 
@@ -35,29 +35,29 @@ const FlowEmergencia = addKeyword(['emergencia']).addAnswer(
 
 function getFaqAnswer(questionNumber) {
     const faqAnswers = {
-        1: "SURA ofrece servicios de atención veterinaria, agendamiento de citas, recomendaciones personalizadas de cuidado y asistencia en emergencias, como traslados a clínicas veterinarias o visitas a domicilio.",
-        2: "Puedes agendar una cita fácilmente a través de este chatbot. Solo necesitarás indicar el tipo de consulta, la especie de tu mascota y la fecha y hora que prefieras.",
-        3: "¡Sí! Si tu mascota necesita atención médica en casa, puedes solicitar una consulta veterinaria a domicilio directamente con nuestro chatbot.",
-        4: "Nuestro chatbot puede ofrecerte recomendaciones generales según la edad, especie y estado de salud de tu mascota. Sin embargo, para un diagnóstico preciso, te recomendamos agendar una cita con uno de nuestros veterinarios.",
-        5: "Si tu mascota enfrenta una emergencia, nuestro chatbot puede coordinar un traslado inmediato a una clínica veterinaria o enviar un profesional a domicilio para asistencia urgente.",
-        6: "¡Por supuesto! Basándonos en la especie, edad, y condición de tu mascota, te ofrecemos recomendaciones personalizadas sobre alimentación, ejercicio y cuidados generales.",
-        7: "Si no estás seguro de qué tipo de atención necesita tu mascota, nuestro chatbot puede hacerte algunas preguntas sobre su comportamiento y síntomas para sugerir el mejor plan de acción.",
-        8: "Sí, dependiendo de las necesidades de tu mascota, nuestro chatbot puede recomendar productos relacionados con la salud, nutrición o el cuidado de tu mascota.",
-        9: "Sí, SURA tiene profesionales capacitados para atender a una amplia variedad de mascotas, incluyendo animales exóticos. Solo necesitas especificar el tipo de mascota al hacer tu consulta.",
-        10: "Nuestro chatbot te permitirá realizar un seguimiento de la salud de tu mascota. Podemos recordarte las próximas citas o proporcionarte información adicional según el tratamiento que haya recibido."
+        1: 'SURA ofrece servicios de atención veterinaria, agendamiento de citas, recomendaciones personalizadas de cuidado y asistencia en emergencias, como traslados a clínicas veterinarias o visitas a domicilio.',
+        2: 'Puedes agendar una cita fácilmente a través de este chatbot. Solo necesitarás indicar el tipo de consulta, la especie de tu mascota y la fecha y hora que prefieras.',
+        3: '¡Sí! Si tu mascota necesita atención médica en casa, puedes solicitar una consulta veterinaria a domicilio directamente con nuestro chatbot.',
+        4: 'Nuestro chatbot puede ofrecerte recomendaciones generales según la edad, especie y estado de salud de tu mascota. Sin embargo, para un diagnóstico preciso, te recomendamos agendar una cita con uno de nuestros veterinarios.',
+        5: 'Si tu mascota enfrenta una emergencia, nuestro chatbot puede coordinar un traslado inmediato a una clínica veterinaria o enviar un profesional a domicilio para asistencia urgente.',
+        6: '¡Por supuesto! Basándonos en la especie, edad, y condición de tu mascota, te ofrecemos recomendaciones personalizadas sobre alimentación, ejercicio y cuidados generales.',
+        7: 'Si no estás seguro de qué tipo de atención necesita tu mascota, nuestro chatbot puede hacerte algunas preguntas sobre su comportamiento y síntomas para sugerir el mejor plan de acción.',
+        8: 'Sí, dependiendo de las necesidades de tu mascota, nuestro chatbot puede recomendar productos relacionados con la salud, nutrición o el cuidado de tu mascota.',
+        9: 'Sí, SURA tiene profesionales capacitados para atender a una amplia variedad de mascotas, incluyendo animales exóticos. Solo necesitas especificar el tipo de mascota al hacer tu consulta.',
+        10: 'Nuestro chatbot te permitirá realizar un seguimiento de la salud de tu mascota. Podemos recordarte las próximas citas o proporcionarte información adicional según el tratamiento que haya recibido.'
     };
 
-    return faqAnswers[questionNumber] || "Lo siento, no tengo una respuesta para esa pregunta.";
+    return faqAnswers[questionNumber] || 'Lo siento, no tengo una respuesta para esa pregunta.';
 }
 
 const flowFAQAux = addKeyword(EVENTS.ACTION)
     .addAnswer(
         [
-            "Ingresa el numero de la pregunta que deseas saber o *volver* para regresar al menu principal!"
+            'Ingresa el numero de la pregunta que deseas saber o *volver* para regresar al menu principal!'
         ],
         { capture: true }, 
         async (ctx, {flowDynamic, gotoFlow})=> {
-            if (ctx.body.includes("volver")){
+            if (ctx.body.includes('volver')){
                 return gotoFlow(flowPrincipal);
             } else {
                 await flowDynamic(getFaqAnswer(parseInt(ctx.body)));
@@ -69,16 +69,18 @@ const flowFAQAux = addKeyword(EVENTS.ACTION)
 const flowFAQ = addKeyword(['faq'])
     .addAnswer(
         [
-            "1. ¿Qué servicios ofrece SURA para el cuidado de mi mascota?",
-            "2. ¿Cómo puedo agendar una cita para mi mascota?",
-            "3. ¿SURA ofrece consultas veterinarias a domicilio?",
-            "4. ¿Cómo puedo saber si mi mascota está saludable?",
-            "5. ¿Qué debo hacer si mi mascota tiene una emergencia?",
-            "6. ¿Puedo recibir recomendaciones personalizadas para el cuidado de mi mascota?",
-            "7. ¿Qué hago si no sé qué tipo de atención necesita mi mascota?",
-            "8. ¿Puedo obtener información sobre productos para mi mascota a través de SURA?",
-            "9. ¿SURA ofrece servicios para mascotas exóticas?",
-            "10. ¿Cómo puedo realizar un seguimiento después de una consulta o tratamiento?"
+            '_*Preguntas frecuentes*_',
+            '',
+            '1. ¿Qué servicios ofrece SURA para el cuidado de mi mascota?',
+            '2. ¿Cómo puedo agendar una cita para mi mascota?',
+            '3. ¿SURA ofrece consultas veterinarias a domicilio?',
+            '4. ¿Cómo puedo saber si mi mascota está saludable?',
+            '5. ¿Qué debo hacer si mi mascota tiene una emergencia?',
+            '6. ¿Puedo recibir recomendaciones personalizadas para el cuidado de mi mascota?',
+            '7. ¿Qué hago si no sé qué tipo de atención necesita mi mascota?',
+            '8. ¿Puedo obtener información sobre productos para mi mascota a través de SURA?',
+            '9. ¿SURA ofrece servicios para mascotas exóticas?',
+            '10. ¿Cómo puedo realizar un seguimiento después de una consulta o tratamiento?'
         ],
         null,
         async (_, { gotoFlow })=> {
